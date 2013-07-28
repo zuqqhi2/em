009fcfd21770af11783e1fd032317260ade82669
Hidetomo Suzuki <zuqqhi2@gmail.com>
2013-07-28 08:19:44 +0000

    Split Query Parameter

--- suggestion-api/routes/index.js
+++ suggestion-api/routes/index.js
@@ -4,6 +4,11 @@
  */
 
 exports.index = function(req, res){
-  res.json([{ author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"}]);
-	//res.render('index', { title: 'Express' });
+  console.log("Query Parameters");
+	console.log(req.query);
+
+  console.log(req.query.uid);
+	
+	//res.json([{ author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"}]);
+	res.render('index', { title: 'Express'});
 };
